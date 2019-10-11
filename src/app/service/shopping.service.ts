70ebf6c45e765c982319d767a89899bdcbd08879
andrey_kir <andrey.kirillov.kiev@gmail.com>
2019-10-11 18:23:09 +0300

    deleted delay

--- src/app/service/shopping.service.ts
+++ src/app/service/shopping.service.ts
@@ -3,7 +3,6 @@ import {HttpClient} from '@angular/common/http';
 
 import {ShoppingItem} from '../store/models/shop-item.model';
 
-import {delay} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 
 
@@ -19,15 +18,15 @@ export class ShoppingService {
 
 
   getShoppingItem(): Observable<any> {
-    return this.http.get(this.SHOPPING_URL).pipe(delay(500));
+    return this.http.get(this.SHOPPING_URL);
   }
 
   addShoppingItem(shoppingItem: ShoppingItem): Observable<any> {
-    return this.http.post(this.SHOPPING_URL, shoppingItem).pipe(delay(500));
+    return this.http.post(this.SHOPPING_URL, shoppingItem);
   }
 
   deleteShoppingItem(id: string): Observable<any> {
-    return this.http.delete(`${this.SHOPPING_URL}/${id}`).pipe(delay(500));
+    return this.http.delete(`${this.SHOPPING_URL}/${id}`);
   }
 
 }
